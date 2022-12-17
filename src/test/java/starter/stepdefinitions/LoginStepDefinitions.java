c6872a5b0c134378339ed15856d72369abaaf0bc
Emanuel Martínez <emanuel.martinez@ceiba.com.co>
2022-12-17 17:23:33 -0500

    UH02 | Replaces definitions in class annotations for LoginStepDefinitions

--- src/test/java/starter/stepdefinitions/LoginStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/LoginStepDefinitions.java
@@ -3,29 +3,30 @@ package starter.stepdefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import net.serenitybdd.screenplay.Actor;
 
 public class LoginStepDefinitions {
 
-    @Given("user is on login page")
-    public void user_is_on_login_page() {
+    @Given("{actor} is on login page")
+    public void marianella_is_on_login_page(Actor actor) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
-    @When("user enter standard_user as username")
-    public void user_enter_standard_user_as_username() {
+    @When("{actor} enter standard_user as username")
+    public void she_enter_standard_user_as_username(Actor actor) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
-    @When("user enter secret_sauce as password")
-    public void user_enter_secret_sauce_as_password() {
+    @When("{actor} enter secret_sauce as password")
+    public void she_enter_secret_sauce_as_password(Actor actor) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
-    
-    @Then("user should be able to login")
-    public void user_should_be_able_to_login() {
+
+    @Then("{actor} should be able to login")
+    public void she_should_be_able_to_login(Actor actor) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
