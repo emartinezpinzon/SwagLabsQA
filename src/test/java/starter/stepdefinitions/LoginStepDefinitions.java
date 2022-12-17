e64ba8218517c3de31cd3f9bef6a69ac745a28c0
Emanuel Martínez <emanuel.martinez@ceiba.com.co>
2022-12-17 17:28:23 -0500

    UH02 | Remove unnecesary code and exceptions to LoginStepDefinitions

--- src/test/java/starter/stepdefinitions/LoginStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/LoginStepDefinitions.java
@@ -9,25 +9,21 @@ public class LoginStepDefinitions {
 
     @Given("{actor} is on login page")
     public void marianella_is_on_login_page(Actor actor) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        
     }
 
     @When("{actor} enter standard_user as username")
     public void she_enter_standard_user_as_username(Actor actor) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
     @When("{actor} enter secret_sauce as password")
     public void she_enter_secret_sauce_as_password(Actor actor) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
     @Then("{actor} should be able to login")
     public void she_should_be_able_to_login(Actor actor) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 }
