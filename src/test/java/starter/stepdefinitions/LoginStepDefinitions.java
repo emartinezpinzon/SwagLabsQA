0aef7f69df40cf76978a53507b8af6523e8fe60a
Emanuel Martínez <emanuel.martinez@ceiba.com.co>
2022-12-17 17:13:51 -0500

    UH02 | Paste step definitions missing reported on console on LoginStepDefinitions class

--- src/test/java/starter/stepdefinitions/LoginStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/LoginStepDefinitions.java
@@ -1,2 +1,32 @@
-package starter.stepdefinitions;public class LoginStepDefinitions {
+package starter.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDefinitions {
+
+    @Given("user is on login page")
+    public void user_is_on_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user enter standard_user as username")
+    public void user_enter_standard_user_as_username() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user enter secret_sauce as password")
+    public void user_enter_secret_sauce_as_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    
+    @Then("user should be able to login")
+    public void user_should_be_able_to_login() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
