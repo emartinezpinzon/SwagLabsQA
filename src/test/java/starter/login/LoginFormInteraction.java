3a29b881fbcf5d0fbf282144adccfbaf95b6ccaf
Emanuel Martínez <emanuel.martinez@ceiba.com.co>
2022-12-18 02:01:03 -0500

    UH02 | Rename methods on LoginFormInteraction

--- src/test/java/starter/login/LoginFormInteraction.java
+++ src/test/java/starter/login/LoginFormInteraction.java
@@ -5,23 +5,23 @@ import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
 
-public class FillData {
+public class LoginFormInteraction {
 
-    public static Performable onUsernameField(String username) {
+    public static Performable fillUsernameField(String username) {
         return Task.where("{0} enter standard_user as username",
                 Enter.theValue(username)
                         .into(LoginForm.USERNAME_FIELD)
         );
     }
 
-    public static Performable onPasswordField(String password) {
+    public static Performable fillPasswordField(String password) {
         return Task.where("{0} enter secret_sauce as password",
                 Enter.theValue(password)
                         .into(LoginForm.PASSWORD_FIELD)
         );
     }
 
-    public static Performable clickEnterButton() {
+    public static Performable clickLoginButton() {
         return Task.where("{0} clicks the login button",
                 Click.on(LoginForm.LOGIN_BUTTON));
     }
