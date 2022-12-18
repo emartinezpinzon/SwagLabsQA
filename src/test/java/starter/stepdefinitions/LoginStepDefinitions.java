530f6016eed3b0a22ceb108a488d7727141f0dc5
Emanuel Martínez <emanuel.martinez@ceiba.com.co>
2022-12-18 02:02:11 -0500

    UH02 | Edit steps on login by user feature and step definitions

--- src/test/java/starter/stepdefinitions/LoginStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/LoginStepDefinitions.java
@@ -4,6 +4,9 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.ensure.Ensure;
+import starter.login.LoginFormInteraction;
+import starter.login.SwagLabsShop;
 import starter.navigation.NavigateTo;
 
 public class LoginStepDefinitions {
@@ -13,18 +16,23 @@ public class LoginStepDefinitions {
         actor.wasAbleTo(NavigateTo.theSwagLabsLoginPage());
     }
 
-    @When("{actor} enter standard_user as username")
-    public void inputUsername(Actor actor) {
-
+    @When("{actor} enter {string} as username")
+    public void inputUsername(Actor actor, String username) {
+        actor.attemptsTo(LoginFormInteraction.fillUsernameField(username));
     }
 
-    @When("{actor} enter secret_sauce as password")
-    public void inputPassword(Actor actor) {
-
+    @When("{actor} enter {string} as password")
+    public void inputPassword(Actor actor, String password) {
+        actor.attemptsTo(LoginFormInteraction.fillPasswordField(password));
     }
 
-    @Then("{actor} should be able to login")
-    public void validateLogin(Actor actor) {
+    @When("{actor} clicks the login button")
+    public void clickEnter(Actor actor) {
+        actor.attemptsTo(LoginFormInteraction.clickLoginButton());
+    }
 
+    @Then("{actor} should be able to see the {string}")
+    public void validateLogin(Actor actor, String products) {
+        actor.attemptsTo(Ensure.that(SwagLabsShop.HEADER_TITLE).hasText(products));
     }
 }
--- src/test/resources/features/login/login_by_user.feature
+++ src/test/resources/features/login/login_by_user.feature
@@ -1,6 +1,7 @@
 Feature: Login to SwagLabs dashboard
   Scenario: Login with valid credentials to standard user
     Given Marianella is on login page
-    When she enter standard_user as username
-    And she enter secret_sauce as password
-    Then she should be able to login
+    When she enter "standard_user" as username
+    And she enter "secret_sauce" as password
+    And she clicks the login button
+    Then she should be able to see the "PRODUCTS"
