d916a414d5a41651a49335c628643804a8aca9da
Emanuel Martínez <emanuel.martinez@ceiba.com.co>
2022-12-18 00:01:00 -0500

    UH02 | Implements step navigateToLogin and rename other methods

--- src/test/java/starter/stepdefinitions/LoginStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/LoginStepDefinitions.java
@@ -4,26 +4,27 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import net.serenitybdd.screenplay.Actor;
+import starter.navigation.NavigateTo;
 
 public class LoginStepDefinitions {
 
     @Given("{actor} is on login page")
-    public void marianella_is_on_login_page(Actor actor) {
-        
+    public void navigateToLogin(Actor actor) {
+        actor.wasAbleTo(NavigateTo.theSwagLabsLoginPage());
     }
 
     @When("{actor} enter standard_user as username")
-    public void she_enter_standard_user_as_username(Actor actor) {
+    public void inputUsername(Actor actor) {
 
     }
 
     @When("{actor} enter secret_sauce as password")
-    public void she_enter_secret_sauce_as_password(Actor actor) {
+    public void inputPassword(Actor actor) {
 
     }
 
     @Then("{actor} should be able to login")
-    public void she_should_be_able_to_login(Actor actor) {
+    public void validateLogin(Actor actor) {
 
     }
 }
