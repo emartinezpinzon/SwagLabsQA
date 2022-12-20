49a26c97eadf161b413a31a21b928af0bd50e8c1
Emanuel Martínez <emanuel.martinez@ceiba.com.co>
2022-12-20 17:22:28 -0500

    UH03 | Add step definitions to add to cart feature

--- src/test/java/starter/stepdefinitions/CartStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/CartStepDefinitions.java
@@ -1,4 +1,32 @@
 package starter.stepdefinitions;
 
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.serenitybdd.screenplay.Actor;
+
 public class CartStepDefinitions {
+    @When("{actor} clicks on {string} article")
+    public void clickOnArticle(Actor actor, String article) {
+
+    }
+
+    @When("{actor} clicks on add to cart button")
+    public void clickOnAddToCartButton(Actor actor) {
+
+    }
+
+    @When("{actor} clicks on cart icon")
+    public void clickOnCartIcon(Actor actor) {
+
+    }
+
+    @Then("{actor} should be to see her cart")
+    public void validateCartView(Actor actor) {
+
+    }
+
+    @Then("{actor} should be to see {string} article")
+    public void validateArticleOnCart(Actor actor, String article) {
+
+    }
 }
