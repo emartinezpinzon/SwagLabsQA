898389fc8c15ccc4e5939955d85fb299171fd34a
Emanuel Martínez <emanuel.martinez@ceiba.com.co>
2022-12-21 11:46:11 -0500

    UH03 | Format class CartDetailContent

--- src/test/java/starter/pages/cartdetail/CartDetailContent.java
+++ src/test/java/starter/pages/cartdetail/CartDetailContent.java
@@ -1,8 +1,5 @@
 package starter.pages.cartdetail;
 
-import net.serenitybdd.screenplay.targets.Target;
-
 public class CartDetailContent {
-    public static Target HEADER_TITLE = Target.the("title").locatedBy(".title");
 
 }
